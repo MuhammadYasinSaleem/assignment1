334c91d655e97b947397f68bdbda86497f6b7537
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-07-15 23:50:12 +0500

    Create Question5.ts

---
+++ Question5.ts
@@ -0,0 +1,5 @@
+let famous_person: string = "Albert Einstein";
+let quote: string = "Imagination is more important than knowledge.";
+let message: string = `"${quote}" - ${famous_person}`;
+
+console.log(message);
