40a1fe3f5257a0f3d9265d0d087ace15c7a641d5
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-07-16 00:35:25 +0500

    Create question13.ts

---
+++ question13.ts
@@ -0,0 +1,6 @@
+let favoriteModeOfTransportation:string[]=['Car','Motorcycle','Bicycle'];
+for(let i=0;i<favoriteModeOfTransportation.length;i++)
+{
+    let message:string=`I would  like to own a ${favoriteModeOfTransportation[i]}.`;
+    console.log(message);
+}
