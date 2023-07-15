075b2869e6ee54bebf94f8dbbb57f81626759a24
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-07-16 00:02:10 +0500

    Create question6.ts

---
+++ question6.ts
@@ -0,0 +1,9 @@
+let name: string = "\t\n   John Doe   \t\n";
+
+console.log("Name with Whitespace:");
+console.log("'" + name + "'");
+
+let strippedName: string = name.trim();
+
+console.log("Name after remove Whitespace:");
+console.log("'" + strippedName + "'");
