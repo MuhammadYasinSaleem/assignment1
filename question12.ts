9e6f739a493c64f6776693b87bbc7f556c9048e1
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-07-16 00:32:14 +0500

    Create question12.ts

---
+++ question12.ts
@@ -0,0 +1,6 @@
+let names:string[]=['haris','Yaseen','Muzammal',];
+for(let i=0;i<names.length;i++)
+{
+    let message:string=`Hello ${names[i]}, how are you today? `;
+    console.log(message);
+}
