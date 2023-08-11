b64a581c2190354a4fa1c340e4579cb2df5a6bdb
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-08-11 23:21:20 +0500

    question14.ts

--- question14.ts
+++ question14.ts
@@ -1,5 +1,5 @@
 let guestList:string[]=['Haris','Abdullah Khalid','Hassan'];
 for(let i=0;i<guestList.length;i++)
 {
-    console.log(`Dear ${guestList[i]} you are cordially invited to dinner at my place. It would be an honor to have you join us.`);
+    console.log(`Dear ${guestList[i]} you are invited to dinner at my place. It would be an honor to have you join us.`);
 }
