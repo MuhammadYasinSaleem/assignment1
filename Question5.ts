25823d8258bdadd106f583e99960eb15d98e6933
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-08-11 22:41:21 +0500

    Question5.ts

--- Question5.ts
+++ Question5.ts
@@ -1,5 +1,4 @@
-let famous_person: string = "Albert Einstein";
-let quote: string = "Imagination is more important than knowledge.";
-let message: string = `"${quote}" - ${famous_person}`;
-
+let famousperson:string="Albert Einstein once said";
+let b:string= "A person who never made a mistake never tried anything new.";
+let message:string=`"${famousperson}", "${b}"`;
 console.log(message);
