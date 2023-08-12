c89017254ee53370f7682d90e99761a18c0b69e9
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-08-12 16:50:24 +0500

    Create Question#19.ts

---
+++ Question#19.ts
@@ -0,0 +1,6 @@
+let guestList:string[]=['Haris','Abdullah Khalid','Hassan'];
+for(let i=0;i<guestList.length;i++)
+{
+    console.log(`Dear ${guestList[i]} you are invited to dinner at my place. It would be an honor to have you join us.`);
+}
+console.log(`no of guests invited to dinner ${guestList.length}`)
