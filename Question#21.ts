b9e0eaecfb9996352b0b4c6c2913794f9e39fdf7
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-08-12 20:05:06 +0500

    Create Question#21.ts

---
+++ Question#21.ts
@@ -0,0 +1,7 @@
+const obj:{name:string,type:string}[]=[
+    {name:'K2',type:'mountain'},
+    {name:'Ravi',type:'river'},
+    {name:'Uk',type:'country'},
+    {name:'Lahore',type:'city'},
+    ];
+    console.log("array of objects ",obj);
