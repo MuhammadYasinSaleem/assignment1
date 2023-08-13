7a8d79726bc679c960b4ef14717a6286e4082d66
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-08-13 23:43:14 +0500

    Create Question#26.ts

---
+++ Question#26.ts
@@ -0,0 +1,20 @@
+let alien_color:string='Green';
+// Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+if(alien_color=='Green')
+{
+    console.log('You earned 5 points for shooting the alien');
+}
+else{
+console.log('You earned 10 points ');
+
+}
+
+alien_color='Red';
+if(alien_color=='Green')
+{
+    console.log('You earned 5 points for shooting the alien');
+}
+else{
+console.log('You earned 10 points ');
+
+}
