188992251578398dbea88de6bc645ce2fb6b5bb5
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-08-13 23:59:28 +0500

    Create Question#28.ts

---
+++ Question#28.ts
@@ -0,0 +1,31 @@
+let age:number=65;
+if(age<2)
+{
+    console.log('Person is a baby');
+}
+
+else if(age>=2&&age<4)
+{
+    console.log('Person is a toddler');
+}
+
+else if(age>=4&&age<13)
+{
+    console.log('Person is a kid');
+}
+
+else if(age>=13&&age<20)
+{
+    console.log('Person is a teenager');
+}
+
+else if(age>=20&&age<65)
+{
+    console.log('Person is a adult');
+}
+
+else 
+{
+    console.log('Person is a elder');
+}
+
