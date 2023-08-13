af2fb4e2cb2124c8ba41d6917f5b789338b75c88
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-08-14 00:32:36 +0500

    Create Question#29.ts

---
+++ Question#29.ts
@@ -0,0 +1,17 @@
+let fav_fruits:string[]=['apple','mango','strawberry'];
+for(let i=0;i<fav_fruits.length;i++)
+{
+if(fav_fruits[i]=='apple')
+{
+console.log('You like apple');
+}
+if(fav_fruits[i]=='mango')
+{
+console.log('You like mango');
+}
+if(fav_fruits[i]=='strawberry')
+{
+console.log('You like strawberry');
+}
+
+}
