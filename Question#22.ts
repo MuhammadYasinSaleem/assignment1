85e916de4be69c71880e7fc214d8ad7dff0209d5
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-08-13 19:51:38 +0500

    Create Question#22.ts

---
+++ Question#22.ts
@@ -0,0 +1,12 @@
+const obj:{name:string,type:string}[]=[
+    {name:'K2',type:'mountain'},
+    {name:'Ravi',type:'river'},
+    {name:'Uk',type:'country'},
+    {name:'Lahore',type:'city'},
+    ];
+    // index error 
+    console.log(obj[4]);
+    //error correction
+    console.log(obj[3]);
+    
+    
