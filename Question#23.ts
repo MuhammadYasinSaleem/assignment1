276e2af3ec885ccd24fffb3f6517033353970fe0
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-08-13 20:02:59 +0500

    Create Question#23.ts

---
+++ Question#23.ts
@@ -0,0 +1,33 @@
+let car:string = 'subaru';
+let no:number=12;
+let studname:string='yasin';
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+
+console.log("is no==12? I predict true");
+console.log(no==12);
+
+console.log("Is no less than 20? I predict true");
+console.log(no<20);
+
+console.log("Is student name is yasin? I predict true");
+console.log(studname=='yasin');
+
+console.log("Is student name not Ali? I predict true");
+console.log(studname!='Ali');
+
+console.log("Is car != 'subaru'? I predict False.")
+console.log(car != 'subaru')
+
+console.log("is no!=12? I predict false");
+console.log(no!=12);
+
+console.log("Is no greater than 20? I predict False");
+console.log(no>20);
+
+console.log("Is student name is not yasin? I predict False");
+console.log(studname!='yasin');
+
+console.log("Is student name is Ali? I predict False");
+console.log(studname=='Ali');
+
