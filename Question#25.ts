236d7dcfb0f806625793ceca4cc6b2c7aee89291
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-08-13 23:36:47 +0500

    Create Question#25.ts

---
+++ Question#25.ts
@@ -0,0 +1,13 @@
+let alien_color:string='Green';
+// Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+if(alien_color=='Green')
+{
+    console.log('You earned 5 points');
+}
+// Write one version of this program that passes the if test and another that fails.
+alien_color='Red';
+if(alien_color=='Green')
+{
+    
+    console.log('You earned 5 points');
+}
