8215a628fa59e6f546db5254e34d166f4a4486a2
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-08-14 18:34:33 +0500

    Create Question#33.ts

---
+++ Question#33.ts
@@ -0,0 +1,25 @@
+let no:number[]=[1,2,3,4,5,6,7,8,9];
+for (let n of no )
+{
+    if(n==1)
+    {
+        console.log(n+'st');
+        
+    }
+    else if(n==2)
+    {
+        console.log(n+'nd');
+        
+    }
+    else if(n==3)
+    {
+        console.log(n+'rd');
+        
+    }
+    else{
+        console.log(n+'th');
+        
+    }
+
+    
+}
