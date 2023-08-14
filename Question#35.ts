9a14e9117a6e65da6476469a72fefcee1ae23ecf
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-08-14 21:44:44 +0500

    Create Question#35.ts

---
+++ Question#35.ts
@@ -0,0 +1,9 @@
+const common = "can fly";
+
+const animals: string[] = ["Eagle", "Parrot", "Sparrow"];
+
+for (const animal of animals) {
+  console.log(`${animal} ${common}.`);
+}
+
+console.log(`Any of these animals would make a great pet!`);
