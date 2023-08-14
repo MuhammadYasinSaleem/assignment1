fbb4dfe6567a2878f19a5cbb918e900536fcbf89
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-08-14 18:50:08 +0500

    Create Question#34.ts

---
+++ Question#34.ts
@@ -0,0 +1,7 @@
+let pizza:string[]=['pepronni','kabab stuffer','chicken'];
+for(const fav_pizza of pizza)
+{
+    console.log(`I love ${fav_pizza} pizza`);
+    
+}
+console.log("I really love pizza!");
