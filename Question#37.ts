72bda572b2b4e9df08539841765808e4f4b95115
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-08-21 17:52:31 +0500

    Create Question#37.ts

---
+++ Question#37.ts
@@ -0,0 +1,7 @@
+function makeShirt(size:string='large',msg:string="I love typescript")
+{
+console.log(`shirt with ${size} and message ${msg}`);
+}
+makeShirt();
+makeShirt('medium');
+makeShirt('small','typescript is my love');
