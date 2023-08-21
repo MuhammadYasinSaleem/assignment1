f39e6e8568fec4968b40da000dd091086703b667
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2023-08-21 18:37:12 +0500

    Create Question#38.ts

---
+++ Question#38.ts
@@ -0,0 +1,7 @@
+function describe_city(city:string,country:string='unknown')
+{
+console.log(`${city} is in ${country}`);
+}
+describe_city('karachi','Pakistan');
+describe_city('Lahore','Pakistan');
+describe_city('Cairo');
